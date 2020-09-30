42f0ae745f689ac167966e875bd4c1f4353ea518
feryal abdullah <feryalabdullah26@gmail.com>
2020-10-01 00:09:41 +0300

    Add the swiper libraries and the slider images

--- js/render.js
+++ js/render.js
@@ -15,8 +15,8 @@ $('.vodiapicker option').each(function () {
     var img = $(this).attr("data-thumbnail");
     var text = this.innerText;
     var value = $(this).val();
-    // var item = '<li><img src="' + img + '" alt="" value="' + value + '"/><span>' + text + '</span></li>';
-    var item = '<li><img src="' + img + '" alt="" value="' + value + '"/>';
+    var item = '<li><img src="' + img + '" alt="" value="' + value + '"/><span>' + text + '</span></li>';
+    //var item = '<li><img src="' + img + '" alt="" value="' + value + '"/>';
     langArray.push(item);
 })
 
@@ -31,8 +31,8 @@ $('#a li').click(function () {
     var img = $(this).find('img').attr("src");
     var value = $(this).find('img').attr('value');
     var text = this.innerText;
-    // var item = '<li><img src="' + img + '" alt="" /><span>' + text + '</span></li>';
-    var item = '<li><img src="' + img + '" alt="" />';
+    var item = '<li><img src="' + img + '" alt="" />  <span>' + text + '</span></li>';
+    //var item = '<li><img src="' + img + '" alt="" />';
     $('.btn-select').html(item);
     $('.btn-select').attr('value', value);
     $(".b").toggle();
@@ -56,3 +56,70 @@ if (sessionLang) {
     $('.btn-select').html(langArray[langIndex]);
     //$('.btn-select').attr('value', 'en');
 }
+
+
+
+var swiper = new Swiper('.product-slider', {
+    speed: 600,
+    spaceBetween: 30,
+    effect: 'slide',
+    loop: true,
+    navigation: {
+        nextEl: '.next',
+        prevEl: '.prev'
+    },
+    on: {
+        init: function () {
+            var index = this.activeIndex;
+
+            var target = $('.product-slider__item').eq(index).data('target');
+
+            console.log(target);
+
+            $('.product-img__item').removeClass('active');
+            $('.product-img__item#' + target).addClass('active');
+        }
+    }
+});
+
+
+//  물품이미지 스와이프   
+swiper.on('slideChange', function () {
+    var index = this.activeIndex;
+
+    var target = $('.product-slider__item').eq(index).data('target');
+
+    console.log(target);
+
+    $('.product-img__item').removeClass('active');
+    $('.product-img__item#' + target).addClass('active');
+
+    if (swiper.isEnd) {
+        $('.prev').removeClass('disabled');
+        $('.next').addClass('disabled');
+    } else {
+        $('.next').removeClass('disabled');
+    }
+
+    if (swiper.isBeginning) {
+        $('.prev').addClass('disabled');
+    } else {
+        $('.prev').removeClass('disabled');
+    }
+});
+//좋아요 효과
+$(".js-fav").on("click", function () {
+    $(this).find('.heart').toggleClass("is-active");
+});
+
+
+var myNav = document.getElementById("nav");
+
+window.onscroll = function () {
+    "use strict";
+    if (document.body.scrollTop >= 100 || document.documentElement.scrollTop >= 100) {
+        myNav.classList.add("scroll");
+    } else {
+        myNav.classList.remove("scroll");
+    }
+};

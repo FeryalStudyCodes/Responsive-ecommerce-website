18f569648f7ba7f899f129e74af25010713ad5e5
feryal-Abdullah <36440504+FeryalStudyCodes@users.noreply.github.com>
2020-09-30 15:03:40 +0300

    Add swiper library to render file

--- js/render.js
+++ js/render.js
@@ -56,3 +56,57 @@ if (sessionLang) {
     $('.btn-select').html(langArray[langIndex]);
     //$('.btn-select').attr('value', 'en');
 }
+
+
+
+var swiper = new Swiper('.product-slider', {
+    speed: 600,
+    spaceBetween: 30,
+    effect: 'slide',        
+    loop: true,
+    navigation: {
+        nextEl: '.next',
+        prevEl: '.prev'
+    },        
+    on: {
+        init: function(){
+            var index = this.activeIndex;
+
+            var target = $('.product-slider__item').eq(index).data('target');
+
+            console.log(target);
+
+            $('.product-img__item').removeClass('active');
+            $('.product-img__item#'+ target).addClass('active');
+        }
+    }
+});
+
+  
+swiper.on('slideChange', function () {
+    var index = this.activeIndex;
+
+    var target = $('.product-slider__item').eq(index).data('target');
+
+    console.log(target);
+
+    $('.product-img__item').removeClass('active');
+    $('.product-img__item#'+ target).addClass('active');
+
+    if(swiper.isEnd) {
+        $('.prev').removeClass('disabled');
+        $('.next').addClass('disabled');
+    } else {
+        $('.next').removeClass('disabled');
+    }
+
+    if(swiper.isBeginning) {
+        $('.prev').addClass('disabled');
+    } else {
+        $('.prev').removeClass('disabled');
+    }
+});
+//좋아요 효과
+$(".js-fav").on("click", function() {
+    $(this).find('.heart').toggleClass("is-active");
+});
